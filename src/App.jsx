ee6371529af2c75c7f7dec39b5f71c31588f65a9
dikshika2901 <dikshikalohar@gmail.com>
2025-11-19 18:20:37 +0530

    cabges diki

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Admin from './pages/Admin'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home/Home'
+import TermsAndConditions from './pages/TermsandCondition'
+import PrivacyPolicy from './pages/privacyandpolicy'
 
 function ProtectedRoute({ children }) {
   const { currentUser } = useAuth()
@@ -41,9 +43,9 @@ function App() {
               <Route 
                 path="/dashboard" 
                 element={
-                  <ProtectedRoute>
+              
                     <Dashboard />
-                  </ProtectedRoute>
+                 
                 } 
               />
               <Route path="/pricing" element={<Pricing />} />
